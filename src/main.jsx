979dd449dc1dbe9795c911f69e942f455e931fa9
lucasmontessoll-blip <lucasmontesso.ll@gmail.com>
2026-05-09 20:55:04 -0300

    Rename App component to app in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx";
-import "./App.css";
+import app from "./app.jsx";
+import "./app.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <app />
   </React.StrictMode>
 );
