5821f1063a8b7f737dcf64a7aa952e3a3ba7ca29
Lucas Montesso <seu-email-real@gmail.com>
2026-05-10 19:13:27 -0300

    Limpando main.jsx de vez

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import App from "./app.jsx";
-import "./App.css";
-=======
-import app from "./app.jsx";
-import "./app.css";
->>>>>>> 86aff55d90fddc1ab43d18a7bdbb2cc40a46ab53
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
