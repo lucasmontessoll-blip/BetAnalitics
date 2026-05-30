6bda567d58114f53105b8340cc5106219407ffb6
Lucas Montesso Coelho <lucasmontesso.ll@gmail.com>
2026-05-30 01:10:54 -0300

    fix: corrigindo a importacao do getFirestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // 🔥 FOI ESTA A IMPORTAÇÃO QUE FALTOU!
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCpD5QjeRK-dWm8DVzOy9RchtgsUSY1E3U",
   authDomain: "srv-d80g2nrbc2fs73f37rf0.firebaseapp.com",
@@ -19,5 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// 🔥 É ESTA LINHA QUE FALTA: Ela "exporta" a ligação para o app.jsx poder usar
+
+// Exporta a ligação do banco de dados para o app.jsx poder usar
 export const db = getFirestore(app);
