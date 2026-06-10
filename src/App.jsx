74b563ed36f2de0fd5486bb413513ed2c89152d0
Lucas Montesso Coelho <lucasmontesso.ll@gmail.com>
2026-06-09 23:37:13 -0300

    atualizacao: descreva aqui o que mudou

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import './app.css'; 
+import './App.css'; 
 import axios from 'axios';
 import { LineChart, Line, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { motion, AnimatePresence } from 'framer-motion';
