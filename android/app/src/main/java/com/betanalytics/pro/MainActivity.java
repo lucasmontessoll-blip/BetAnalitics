fb45431b88f8cf9be3aff41a9a5b21454c07805c
Lucas Montesso Coelho <lucasmontesso.ll@gmail.com>
2026-07-18 17:23:03 -0300

    fix: adiciona dependencia capacitor app para voltar nativo

--- android/app/src/main/java/com/betanalytics/pro/MainActivity.java
+++ android/app/src/main/java/com/betanalytics/pro/MainActivity.java
@@ -1,5 +1,24 @@
-package com.betanalytics.pro;
+﻿package com.betanalytics.pro;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onBackPressed() {
+    try {
+      if (this.bridge != null && this.bridge.getWebView() != null) {
+        this.bridge.getWebView().post(new Runnable() {
+          @Override
+          public void run() {
+            MainActivity.this.bridge.getWebView().evaluateJavascript("window.dispatchEvent(new Event('betAndroidBackButton'));", null);
+          }
+        });
+        return;
+      }
+    } catch (Exception e) {
+      // Fallback seguro
+    }
+
+    super.onBackPressed();
+  }
+}
