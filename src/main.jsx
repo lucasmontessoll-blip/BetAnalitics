682e8c8cd2edceb6bf339284bfade635d1b89ca4
Lucas Montesso Coelho <lucasmontesso.ll@gmail.com>
2026-07-27 22:26:13 -0300

    feat: adiciona entrada comercial com termos login e cadastro

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import EntradaComercialGate from './components/EntradaComercialGate.jsx';
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 import './App.css';
 import { HashRouter } from 'react-router-dom';
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
       <HashRouter>
-      <App />
+      <EntradaComercialGate>
+        <App />
+      </EntradaComercialGate>
     </HashRouter>
     </ErrorBoundary>
   </React.StrictMode>,
