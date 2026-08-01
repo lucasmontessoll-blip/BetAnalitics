32596f03db11df7c699c152e6556f4d5620d3074
Lucas Montesso Coelho <lucasmontesso.ll@gmail.com>
2026-08-01 00:41:53 -0300

    security: hardening de producao etapa 35B

--- src/services/teste_banco.py
+++ src/services/teste_banco.py
@@ -1,8 +1,9 @@
+import os
 from supabase import create_client, Client
 
 # 1. Configuração da Conexão
 SUPABASE_URL = 'https://pztznppbmonhrrzfbnvh.supabase.co'
-SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6dHpucHBibW9uaHJyemZibnZoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDYxNzA5MiwiZXhwIjoyMDk2MTkzMDkyfQ.t3Dvvcc6jpa9joGECZmIX4QigdjlseuVjDhiLU_Q0mY' # <--- Cole sua chave anon/public aqui
+SUPABASE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY", "")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
