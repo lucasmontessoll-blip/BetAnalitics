43329c1cc8ef776d593d9cb1eb668a761c0aa32f
Lucas Montesso Coelho <lucasmontesso.ll@gmail.com>
2026-09-07 22:27:14 -0300

    fix(release): harden Android dependencies and repo outputs

--- android/app/src/main/java/com/betanalytics/pro/MainActivity.java
+++ android/app/src/main/java/com/betanalytics/pro/MainActivity.java
@@ -3,22 +3,4 @@ package com.betanalytics.pro;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onBackPressed() {
-    try {
-      if (this.bridge != null && this.bridge.getWebView() != null) {
-        this.bridge.getWebView().post(new Runnable() {
-          @Override
-          public void run() {
-            MainActivity.this.bridge.getWebView().evaluateJavascript("window.dispatchEvent(new Event('betAndroidBackButton'));", null);
-          }
-        });
-        return;
-      }
-    } catch (Exception e) {
-      // fallback seguro
-    }
-
-    super.onBackPressed();
-  }
 }
